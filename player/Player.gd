49278cf3e73e8302627fc2018e35112b0efc845d
requiem <z.nelson13@gmail.com>
2024-04-25 19:29:49 -0400

    Added particle effects to fire wand

--- player/Player.gd
+++ player/Player.gd
@@ -6,6 +6,7 @@ const gravity = 320
 
 onready var anim_player = $AnimationPlayer
 onready var raycast = $RayCast
+onready var fire_particles = $CanvasLayer/Control/Particles2D
 
 onready var player_health = 5
 var equipped_weapon
@@ -67,10 +68,13 @@ func _physics_process(delta):
 	if self.equipped_weapon == "firewand":
 		if Input.is_action_pressed("shoot"):
 			anim_player.play("shoot")
+			
 
 			emit_signal("gun_fired")
 
 			$CanvasLayer/Control/FireWand.set_offset(Vector2(rand_range(-5,5),rand_range(-5,5)))
+
+			fire_particles.emitting = true
 
 			$crossbowSound.pitch_scale = rand_range(0.9,1.1)
 			$crossbowSound.play()
@@ -80,6 +84,7 @@ func _physics_process(delta):
 				raycast.get_collider().recieve_damage()
 		else:
 			anim_player.play("idle")
+			fire_particles.emitting = false
 			$CanvasLayer/Control/FireWand.set_offset(Vector2(0,0))
 
 	elif Input.is_action_pressed("shoot") and !anim_player.is_playing() and self.equipped_weapon == "crossbow":
